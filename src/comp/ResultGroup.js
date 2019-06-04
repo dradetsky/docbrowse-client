23066ec05cadfc0ffba94628ece1a6d02b66941b
Daniel Radetsky <dradetsky@gmail.com>
2019-06-04 14:58:35 -0700

    try columns

--- src/comp/ResultGroup.js
+++ src/comp/ResultGroup.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import Columned from 'react-columned'
 
 import ResultItem from './ResultItem'
 
 class ResultGroup extends React.Component {
-  render () {
+  items () {
     let items = this.props.data.map((data, idx) => {
       let key = idx
       return (<ResultItem key={key} data={data} />)
     })
-    return (<div>{items}</div>)
+    return items
+  }
+
+  normal () {
+    return (<div>{this.items()}</div>)
+  }
+
+  columned () {
+    return (
+      <Columned columns={3} >
+        {this.items()}
+      </Columned>
+    )
+  }
+
+  render () {
+    return this.columned()
   }
 }
 
